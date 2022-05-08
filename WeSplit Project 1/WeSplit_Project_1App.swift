962d310d82fa08c425cd237537aceb9bf8acd96e
Pavel Timofeev <ttt.1.12@bk.ru>
2022-05-08 18:51:18 +0700

    Initial Commit

---
+++ WeSplit Project 1/WeSplit_Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_Project_1App.swift
+//  WeSplit Project 1
+//
+//  Created by Павел Тимофеев on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
